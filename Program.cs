e240d9239fa932dc1a1102a6e25d5a9ca0f6c7ca
Yulia Gorbulina <yulya.gorbulina@gmail.com>
2021-10-17 16:40:02 +0300

    hello everyone

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace Console_App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello Everyone!");
             Console.ReadKey();
         }
     }
